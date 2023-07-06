08fc36580e1a1d859edd4d0a520cf4c4e24b9987
leehjhjhj <tsukiakarii@naver.com>
2023-07-06 13:36:19 +0900

    7/6 ToMany join

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -74,4 +74,27 @@ public class OrderRepository {
                         "join o.delivery d ", OrderSimpleQueryDto.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                        "select distinct o from Order o " +
+                                "join fetch o.member m " +
+                                "join fetch o.delivery d " +
+                                "join fetch o.orderItems oi " +
+                                "join fetch oi.item i", Order.class)
+                .getResultList();
+    } // distinct 는 db에 'distinct' 알려줌, 엔티티 id가 중복이면 걸러서 알려준다. -> 쿼리 한번 나감
+    // 그러나 페이징이 붏가능하다. 두개를 가지고 와야되는데 4개가 가져와버림. 페이징은 어떻게하지? 그래서
+    // 오더에대한 사이즈가 안되게 된다.(limit). 일대다에서는 fetch join 하면 페이징이 안된다.
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select o from Order o " +
+                                "join fetch o.member m " +
+                                "join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+    // To One 관계는 fetch join 해두고, default_batch_fetch_size: 100 를 설정한다
+    // 그리고 컬렉션인 경우 @BatchSize로 적는다. 그냥 근데 yml에서 설정한다.
 }
